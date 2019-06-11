66c0da093758b93dca3704425c8c50d39aa6d160
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-11 12:56:17 +0100

    temporary_pages_users_organisation

---
+++ E4S/Controllers/OrganisationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E4S.Controllers
+{
+  public class OrganisationController : Controller
+  {
+    public OrganisationController()
+    {
+
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Branch()
+    {
+      return View();
+    }
+  }
+}
