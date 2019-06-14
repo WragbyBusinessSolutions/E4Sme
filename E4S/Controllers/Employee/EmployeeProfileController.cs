e21a132508c4f0f41ee9e6e021819c25498fe8d6
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-14 13:27:10 +0100

    employeelayout

---
+++ E4S/Controllers/Employee/EmployeeProfileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E4S.Controllers.Employee
+{
+    public class EmployeeProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    public IActionResult PersonalDetails()
+    {
+      return View();
+    }
+
+    public IActionResult ContactDetails()
+    {
+      return View();
+    }
+
+    public IActionResult EmergencyContacts()
+    {
+      return View();
+    }
+
+  }
+}
