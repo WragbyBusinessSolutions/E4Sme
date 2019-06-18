c62baf1ba58e2eadd439d8336e658b629aaafba7
Emmanuel Temofe Anene <Eanene@wragbysolutions.com>
2019-06-18 11:33:27 +0100

    Job Posting to database

---
+++ E4S/ViewModel/PostNewJobCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E4S.ViewModel
+{
+    public class PostNewJobCategory
+    {
+        public string JobCategory { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ E4S/ViewModel/PostNewJobTitle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E4S.ViewModel
+{
+    public class PostNewJobTitle
+    {
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+    }
+}
