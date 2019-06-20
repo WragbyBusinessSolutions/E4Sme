568dcec40b8fb60469db44fc96012895544bf6d7
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-20 17:15:30 +0100

    employeeDetails

--- E4S/Controllers/Employee/EmployeeProfileController.cs
+++ E4S/Controllers/Employee/EmployeeProfileController.cs
@@ -1,31 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using E4S.Data;
+using E4S.Models;
+using E4S.Models.HumanResource;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E4S.Controllers.Employee
 {
+  [Authorize]
     public class EmployeeProfileController : Controller
     {
-        public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public EmployeeProfileController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+      _context = context;
+      _userManager = userManager;
+
+    }
+
+    public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult PersonalDetails()
+    private Guid getOrg()
+    {
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      var orgId = _context.Users.Where(x => x.Id == userId).FirstOrDefault().OrganisationId;
+
+      return orgId;
+    }
+
+
+    public IActionResult PersonalDetails()
         {
-          return View();
+      var orgId = getOrg();
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+      var employeeDetails = _context.EmployeeDetails.Where(x => x.UserId == Guid.Parse(userId)).FirstOrDefault();
+
+          return View(employeeDetails);
         }
+
+    [HttpPost]
+    public async Task<IActionResult> PersonalDetails(EmployeeDetail employeeDetail)
+    {
+      if(employeeDetail == null)
+      {
+        return NotFound();
+      }
+
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      employeeDetail.UserId = Guid.Parse(userId);
+      _context.Update(employeeDetail);
+      await _context.SaveChangesAsync();
+
+
+      return View(employeeDetail);
+    }
 
         public IActionResult ContactDetails()
         {
-          return View();
+
+      var orgId = getOrg();
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      var employeeDetails = _context.EmployeeDetails.Where(x => x.UserId == Guid.Parse(userId)).FirstOrDefault();
+
+      var contactDetails = _context.ContactDetails.Where(x => x.EmployeeDetailId == employeeDetails.Id).FirstOrDefault();
+
+      if (contactDetails == null)
+      {
+        return View();
+      }
+
+
+      return View(contactDetails);
         }
 
-        public IActionResult EmergencyContacts()
+    [HttpPost]
+    public async Task<IActionResult> ContactDetails(ContactDetail contactDetail)
+    {
+      if (contactDetail == null)
+      {
+        return NotFound();
+      }
+      var orgId = getOrg();
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      var employeeDetails = _context.EmployeeDetails.Where(x => x.UserId == Guid.Parse(userId)).FirstOrDefault();
+
+      contactDetail.EmployeeDetailId = employeeDetails.Id;
+      contactDetail.OrganisationId = orgId;
+      contactDetail.IsActive = true;
+
+      _context.Update(contactDetail);
+      await _context.SaveChangesAsync();
+
+      return View();
+    }
+
+
+    public IActionResult EmergencyContacts()
         {
-          return View();
+
+      var orgId = getOrg();
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      var employeeDetails = _context.EmployeeDetails.Where(x => x.UserId == Guid.Parse(userId)).FirstOrDefault();
+
+      var emergencyContacts = _context.EmergencyContacts.Where(x => x.EmployeeDetailId == employeeDetails.Id).ToList();
+
+      //if(emergencyContacts == null)
+      //{
+      //  return View(emergencyContacts);
+      //}
+
+      return View(emergencyContacts);
         }
 
         public IActionResult Dependents()
