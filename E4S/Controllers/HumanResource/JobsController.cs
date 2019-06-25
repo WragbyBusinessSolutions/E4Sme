2b9f54d9c3792aa01a81cac4425ba3fcb3120901
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-25 16:55:11 +0100

    leavedone

--- E4S/Controllers/HumanResource/JobsController.cs
+++ E4S/Controllers/HumanResource/JobsController.cs
@@ -13,369 +13,392 @@ using Microsoft.EntityFrameworkCore;
 
 namespace E4S.Controllers.HumanResource
 {
-    public class JobsController : Controller
+  public class JobsController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public JobsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
+      _context = context;
+      _userManager = userManager;
 
-        public JobsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager )
-        {
-            _context = context;
-            _userManager = userManager;
+    }
 
-        }
+    private Guid getOrg()
+    {
+      var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+      var orgId = _context.Users.Where(x => x.Id == userId).FirstOrDefault().OrganisationId;
 
-        private Guid getOrg()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var orgId = _context.Users.Where(x => x.Id == userId).FirstOrDefault().OrganisationId;
+      return orgId;
+    }
+    public IActionResult Index()
+    {
+      return View();
+    }
 
-            return orgId;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult JobTitle()
-        {
+    public IActionResult JobTitle()
+    {
       var orgId = getOrg();
       var jobTitles = _context.JobTitles.Where(x => x.OrganisationId == orgId).ToList();
-          return View(jobTitles);
-        }
+      return View(jobTitles);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> PostNewJobTitle([FromBody]PostNewJobTitle postNewJobTitle)
+    [HttpPost]
+    public async Task<IActionResult> PostNewJobTitle([FromBody]PostNewJobTitle postNewJobTitle)
+    {
+      if (postNewJobTitle == null)
+      {
+        return Json(new
         {
-            if (postNewJobTitle == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
-
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                JobTitle newJobTitle = new JobTitle()
-                {
-                    Id = Guid.NewGuid(),
-                    JobTitleName = postNewJobTitle.JobTitle,
-                    Description = postNewJobTitle.Description,
-                    Note = postNewJobTitle.Note,
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newJobTitle);
-                _context.SaveChanges();
-
-
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
+          msg = "No Data"
         }
+       );
+      }
 
+      var orgId = getOrg();
+      var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+      int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
 
-        public IActionResult EmploymentStatus()
+      try
+      {
+        JobTitle newJobTitle = new JobTitle()
         {
+          Id = Guid.NewGuid(),
+          JobTitleName = postNewJobTitle.JobTitle,
+          Description = postNewJobTitle.Description,
+          Note = postNewJobTitle.Note,
+          OrganisationId = orgId
+        };
+
+        _context.Add(newJobTitle);
+        _context.SaveChanges();
+
+
+        return Json(new
+        {
+          msg = "Success"
+        }
+     );
+      }
+      catch (Exception ee)
+      {
+
+      }
+
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
+    }
+
+
+    public IActionResult EmploymentStatus()
+    {
       var orgId = getOrg();
       var employmentStatuses = _context.EmploymentStatuses.Where(x => x.OrganisationId == orgId).ToList();
 
       return View(employmentStatuses);
-        }
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> PostNewEmploymentStatus([FromBody]PostNewEmploymentStatus postNewEmploymentStatus)
+    [HttpPost]
+    public async Task<IActionResult> PostNewEmploymentStatus([FromBody]PostNewEmploymentStatus postNewEmploymentStatus)
+    {
+      if (postNewEmploymentStatus == null)
+      {
+        return Json(new
         {
-            if (postNewEmploymentStatus == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
-
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                EmploymentStatus newEmploymentStatus = new EmploymentStatus()
-                {
-                    Id = Guid.NewGuid(),
-                    EmploymentStatusName = postNewEmploymentStatus.EmploymentStatusName,
-                    Description = postNewEmploymentStatus.Description,
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newEmploymentStatus);
-                _context.SaveChanges();
-
-
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
+          msg = "No Data"
         }
+       );
+      }
 
-        public IActionResult JobCategory()
+      var orgId = getOrg();
+      var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+      int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
+
+      try
+      {
+        EmploymentStatus newEmploymentStatus = new EmploymentStatus()
         {
+          Id = Guid.NewGuid(),
+          EmploymentStatusName = postNewEmploymentStatus.EmploymentStatusName,
+          Description = postNewEmploymentStatus.Description,
+          OrganisationId = orgId
+        };
+
+        _context.Add(newEmploymentStatus);
+        _context.SaveChanges();
+
+
+        return Json(new
+        {
+          msg = "Success"
+        }
+     );
+      }
+      catch (Exception ee)
+      {
+
+      }
+
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
+    }
+
+    public IActionResult JobCategory()
+    {
       var orgId = getOrg();
       var jobCategory = _context.JobCategories.Where(x => x.OrganisationId == orgId).ToList();
 
-          return View(jobCategory);
-        }
+      return View(jobCategory);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> PostNewJobCategory([FromBody]PostNewJobCategory postNewJobCategory)
+    [HttpPost]
+    public async Task<IActionResult> PostNewJobCategory([FromBody]PostNewJobCategory postNewJobCategory)
+    {
+      if (postNewJobCategory == null)
+      {
+        return Json(new
         {
-            if (postNewJobCategory == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
-
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                JobCategory newJobCategory = new JobCategory()
-                {
-                    Id = Guid.NewGuid(),
-                    JobCategoryName = postNewJobCategory.JobCategory,
-                    Description = postNewJobCategory.Description,
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newJobCategory);
-                _context.SaveChanges();
-
-
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
+          msg = "No Data"
         }
+       );
+      }
 
-        public IActionResult PayGrade()
+      var orgId = getOrg();
+      var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+      int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
+
+      try
+      {
+        JobCategory newJobCategory = new JobCategory()
         {
+          Id = Guid.NewGuid(),
+          JobCategoryName = postNewJobCategory.JobCategory,
+          Description = postNewJobCategory.Description,
+          OrganisationId = orgId
+        };
+
+        _context.Add(newJobCategory);
+        _context.SaveChanges();
+
+
+        return Json(new
+        {
+          msg = "Success"
+        }
+     );
+      }
+      catch (Exception ee)
+      {
+
+      }
+
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
+    }
+
+    public IActionResult PayGrade()
+    {
       var orgId = getOrg();
       var payGrades = _context.PayGrades.Where(x => x.OrganisationId == orgId).ToList();
 
       return View(payGrades);
-        }
-         [HttpPost]
-         public async Task<IActionResult> PostNewPayGrade([FromBody]PostNewPayGrade postNewPayGrade)
-         {
-            if (postNewPayGrade == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
-
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                PayGrade newPayGrade = new PayGrade()
-                {
-                    Id = Guid.NewGuid(),
-                    PayGradeName = postNewPayGrade.PayGradeName,
-                    Currency = postNewPayGrade.Currency,
-                    MinimumSalary = postNewPayGrade.MinimumSalary,
-                    MaximumSalary = postNewPayGrade.MaximumSalary,
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newPayGrade);
-                _context.SaveChanges();
-
-
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
-         }
-
-        public IActionResult Department()
+    }
+    [HttpPost]
+    public async Task<IActionResult> PostNewPayGrade([FromBody]PostNewPayGrade postNewPayGrade)
+    {
+      if (postNewPayGrade == null)
+      {
+        return Json(new
         {
-            var orgId = getOrg();
-            var department = _context.Departments.Where(x => x.OrganisationId == orgId).ToList();
-
-            return View(department);
+          msg = "No Data"
         }
+       );
+      }
 
-        [HttpPost]
-        public async Task<IActionResult> Department([FromBody]PostNewDepartment postNewDepartment)
+      var orgId = getOrg();
+      var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+      int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
+
+      try
+      {
+        PayGrade newPayGrade = new PayGrade()
         {
-            if (postNewDepartment == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
+          Id = Guid.NewGuid(),
+          PayGradeName = postNewPayGrade.PayGradeName,
+          Currency = postNewPayGrade.Currency,
+          MinimumSalary = postNewPayGrade.MinimumSalary,
+          MaximumSalary = postNewPayGrade.MaximumSalary,
+          OrganisationId = orgId
+        };
 
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                Department newDepartment = new Department()
-                {
-                    Id = Guid.NewGuid(),
-                    DepartmentName = postNewDepartment.DepartmentName,
-                    Description = postNewDepartment.Description,
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newDepartment);
-                _context.SaveChanges();
+        _context.Add(newPayGrade);
+        _context.SaveChanges();
 
 
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
-        }
-
-        public IActionResult LeaveConfiguration()
+        return Json(new
         {
-            var orgId = getOrg();
-            var LeaveConfiguration = _context.LeaveConfigurations.Where(x => x.OrganisationId == orgId).ToList();
-
-            return View(LeaveConfiguration);
+          msg = "Success"
         }
+     );
+      }
+      catch (Exception ee)
+      {
 
-        [HttpPost]
-        public async Task<IActionResult> LeaveConfiguration([FromBody]PostNewLeaveConfiguration postNewLeaveConfiguration)
-        {
-            if (postNewLeaveConfiguration == null)
-            {
-                return Json(new
-                {
-                    msg = "No Data"
-                }
-               );
-            }
+      }
 
-            var orgId = getOrg();
-            var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
-            int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
-
-            try
-            {
-                LeaveConfiguration newLeaveConfiguration = new LeaveConfiguration()
-                {
-                    Id = Guid.NewGuid(),
-                    LeaveTitle = postNewLeaveConfiguration.LeaveTitle,
-                    Description = postNewLeaveConfiguration.Description,
-                    MaxDuration = postNewLeaveConfiguration.MaxDuration,
-                    MaxApplication = postNewLeaveConfiguration.MaxApplication,                   
-                    OrganisationId = orgId
-                };
-
-                _context.Add(newLeaveConfiguration);
-                _context.SaveChanges();
-
-
-                return Json(new
-                {
-                    msg = "Success"
-                }
-             );
-            }
-            catch (Exception ee)
-            {
-
-            }
-
-            return Json(
-            new
-            {
-                msg = "Fail"
-            });
-        }
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
     }
 
+    public IActionResult Department()
+    {
+      var orgId = getOrg();
+      var department = _context.Departments.Where(x => x.OrganisationId == orgId).ToList();
+
+      return View(department);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Department([FromBody]PostNewDepartment postNewDepartment)
+    {
+      if (postNewDepartment == null)
+      {
+        return Json(new
+        {
+          msg = "No Data"
+        }
+       );
+      }
+
+      var orgId = getOrg();
+      var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+      int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
+
+      try
+      {
+        Department newDepartment = new Department()
+        {
+          Id = Guid.NewGuid(),
+          DepartmentName = postNewDepartment.DepartmentName,
+          Description = postNewDepartment.Description,
+          OrganisationId = orgId
+        };
+
+        _context.Add(newDepartment);
+        _context.SaveChanges();
+
+
+        return Json(new
+        {
+          msg = "Success"
+        }
+     );
+      }
+      catch (Exception ee)
+      {
+
+      }
+
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
+    }
+
+    public IActionResult LeaveConfiguration()
+    {
+      var orgId = getOrg();
+      var LeaveConfiguration = _context.LeaveConfigurations.Where(x => x.OrganisationId == orgId).ToList();
+
+      return View(LeaveConfiguration);
+    }
+
+    //[HttpPost]
+    //public async Task<IActionResult> LeaveConfiguration(LeaveConfiguration leaveConfiguration)
+    //{
+    //  var orgId = getOrg();
+
+    //  if (leaveConfiguration != null)
+    //  {
+    //    leaveConfiguration.OrganisationId = orgId;
+    //    leaveConfiguration.Id = Guid.NewGuid();
+
+    //    _context.Add(leaveConfiguration);
+    //    await _context.SaveChangesAsync();
+
+    //    return RedirectToAction("LeaveConfiguration");
+
+    //  }
+
+
+    //  return RedirectToAction("LeaveConfiguration");
+    //}
+
+  
+  [HttpPost]
+  public async Task<IActionResult> LeaveConfiguration([FromBody]PostNewLeaveConfiguration postNewLeaveConfiguration)
+  {
+    if (postNewLeaveConfiguration == null)
+    {
+      return Json(new
+      {
+        msg = "No Data"
+      }
+     );
+    }
+
+    var orgId = getOrg();
+    var organisationDetails = await _context.Organisations.Where(x => x.Id == orgId).FirstOrDefaultAsync();
+    int noOfEmployee = _context.Users.Where(x => x.OrganisationId == orgId).Count();
+
+    try
+    {
+      LeaveConfiguration newLeaveConfiguration = new LeaveConfiguration()
+      {
+        Id = Guid.NewGuid(),
+        LeaveTitle = postNewLeaveConfiguration.LeaveTitle,
+        Description = postNewLeaveConfiguration.Description,
+        MaxDuration = postNewLeaveConfiguration.MaxDuration,
+        MaxApplication = postNewLeaveConfiguration.MaxApplication,
+        OrganisationId = orgId
+      };
+
+      _context.Add(newLeaveConfiguration);
+      _context.SaveChanges();
+
+
+      return Json(new
+      {
+        msg = "Success"
+      }
+   );
+    }
+    catch (Exception ee)
+    {
+
+    }
+
+    return Json(
+    new
+    {
+      msg = "Fail"
+    });
+  }
+
+
+}
 }
