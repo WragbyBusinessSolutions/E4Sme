799ff0ddd53bb07e44f6e930b26b31347e95c3f8
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-25 20:45:13 +0100

    companyAsset

--- E4S/Data/Migrations/20190625184208_orgAsset.Designer.cs
+++ E4S/Data/Migrations/20190625184208_orgAsset.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace E4S.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190625184208_orgAsset")]
+    partial class orgAsset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
