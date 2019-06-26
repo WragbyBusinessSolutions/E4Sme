2575bf7bff7cef06ca5c9cbde41640f365c2cf1b
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-06-26 11:38:02 +0100

    leaveapproval

--- E4S/Data/Migrations/20190626092509_leaveupdate.Designer.cs
+++ E4S/Data/Migrations/20190626092509_leaveupdate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace E4S.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190626092509_leaveupdate")]
+    partial class leaveupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
