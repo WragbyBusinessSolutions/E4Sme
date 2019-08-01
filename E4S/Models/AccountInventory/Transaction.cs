5fb104b9457bf37b264d2c2d3c385840899d8ce1
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-08-01 11:02:49 +0100

    latest

---
+++ E4S/Models/AccountInventory/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E4S.Models.AccountInventory
+{
+  public class Transaction : BaseClass
+  {
+    public Guid Id { get; set; }
+
+    public string Description { get; set; }
+    public string Type { get; set; }
+
+    public float Amount { get; set; }
+   // public 
+  }
+}
