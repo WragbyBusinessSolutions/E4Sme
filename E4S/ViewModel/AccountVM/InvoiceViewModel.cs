679a4376c276571c394c5bd890abdfb23e2fe4ff
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-08-08 18:43:28 +0100

    get updates

---
+++ E4S/ViewModel/AccountVM/InvoiceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using E4S.Models;
+using E4S.Models.AccountInventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E4S.ViewModel.AccountVM
+{
+  public class InvoiceViewModel
+  {
+    public Guid Id { get; set; }
+    public int QuoteNo { get; set; }
+
+    public Customer Customer { get; set; }
+    public Organisation Organisation { get; set; }
+
+    public List<InvoiceItem> QuoteItems { get; set; }
+
+    public string InoviceTitle { get; set; }
+    public float Tax { get; set; }
+    public float SubTotal { get; set; }
+    public float Total { get; set; }
+
+  }
+}
